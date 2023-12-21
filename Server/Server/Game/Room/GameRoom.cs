0d1b26cdf9d54cc8e6e914519ca846574a717ff9
HumbleBird <wltnsdid67@naver.com>
2023-12-21 16:25:36 +0900

    Die Effect

--- Server/Server/Game/Room/GameRoom.cs
+++ Server/Server/Game/Room/GameRoom.cs
@@ -51,6 +51,8 @@ namespace Server.Game
                     _players.Add(gameObject.Id, player);
                     player.Room = this;
 
+                    Map.ApplyMove(player, new Vector2Int(player.CellPos.x, player.CellPos.y));
+
                     // 본인한테 정보 전송
                     {
                         S_EnterGame enterPacket = new S_EnterGame();
@@ -64,6 +66,12 @@ namespace Server.Game
                                 spawnPacket.Objects.Add(p.Info);
                         }
 
+                        foreach (Monster m in _monsters.Values)
+                                spawnPacket.Objects.Add(m.Info);
+
+                        foreach (Projectile p in _projectiles.Values)
+                                spawnPacket.Objects.Add(p.Info);
+
                         player.Session.Send(spawnPacket);
                     }
                 }
@@ -73,6 +81,9 @@ namespace Server.Game
 
                     _monsters.Add(gameObject.Id, monster);
                     monster.Room = this;
+
+                    Map.ApplyMove(monster, new Vector2Int(monster.CellPos.x, monster.CellPos.y));
+
                 }
                 else if (type == GameObjectType.Projectile)
                 {
